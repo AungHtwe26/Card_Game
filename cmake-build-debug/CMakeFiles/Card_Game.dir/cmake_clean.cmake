f9247bcedea40b066a0d276328875c5b389bc731
AungHtwe26 <aunghtwe.ah526@gmail.com>
2022-12-31 14:03:19 +0630

    v1

---
+++ cmake-build-debug/CMakeFiles/Card_Game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Card_Game.dir/main.c.obj"
+  "CMakeFiles/Card_Game.dir/main.c.obj.d"
+  "Card_Game.exe"
+  "Card_Game.exe.manifest"
+  "Card_Game.pdb"
+  "libCard_Game.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Card_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Card_Game.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Card_Game.
+# This may be replaced when dependencies are built.
